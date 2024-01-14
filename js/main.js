83550b0914539b116177650b1aeaa0d764182170
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-14 15:22:12 +0000

    completed design - bootstrap - scss - js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.menu').click(function(e) {
+		e.stopPropagation();
+		$('#main_nav').toggleClass('active');
+	});
+
+	$(document).click(function() {
+		$('#main_nav').removeClass('active');
+	});
+});
